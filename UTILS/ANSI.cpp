893de8e5006c9cb3697d52505232933886ed3126
John Grzegorczyk <jtgrzeg@gmail.com>
2024-10-17 21:13:44 -0500

    Functions for the ANSI class to build and print commands.

--- UTILS/ANSI.cpp
+++ UTILS/ANSI.cpp
@@ -1,2 +1,40 @@
 #include "ANSI.hpp"
 
+namespace ANSI
+{
+#define VALUE_STEP 51 // = 255 / 5
+    BYTE BYTE_COLOR::rgb_code( BYTE __red, BYTE __green, BYTE __blue )
+    { return ( ( __red / VALUE_STEP ) * RGB::RED_INC ) + ( ( __green / VALUE_STEP ) * RGB::GREEN_INC ) + ( ( __blue / VALUE_STEP ) * RGB::BLUE_INC ); }
+    
+    COMMAND::COMMAND( EFFECT __effect ) : codes( { (BYTE) __effect } ), closing_tag( CODE::END::TEXT ) {}
+    COMMAND::COMMAND( LAYER __layer, BASIC_COLOR __color ) : closing_tag( CODE::END::TEXT )
+    {
+        if ( __layer == LAYER::UNDERLINE && __color != BASIC_COLOR::DEFAULT )
+            throw std::invalid_argument( "COMMAND( LAYER, BASIC_COLOR ) can only take the argument of BASIC_COLOR::DEFAULT when the layer is LAYER::UNDERLINE!" );
+        codes.push_back( (BYTE)__layer + (BYTE)__color );
+    }
+    COMMAND::COMMAND( LAYER __layer, BYTE_COLOR __color ) : codes( { (BYTE)__layer, CODE::BYTE_FORMAT, __color._code } ), closing_tag( CODE::END::TEXT ) {}
+    COMMAND::COMMAND( LAYER __layer, RGB_COLOR __color ) : codes( { (BYTE)__layer, CODE::RGB_FORMAT, __color._red, __color._green, __color._blue } ), closing_tag( CODE::END::TEXT ) {}
+
+    std::ostream& operator << ( std::ostream& output, const ANSI::COMMAND& command )
+    {
+        output << ANSI::COMMAND::CODE::BEGIN_CODE;
+        for ( const BYTE& code : command.codes )
+            output << code;
+        output << command.closing_tag;
+        return output;
+    }
+    std::ostream& operator << ( std::ostream& output, ANSI::COMMAND&& command )
+    {
+        output << ANSI::COMMAND::CODE::BEGIN::ESCAPE << ANSI::COMMAND::CODE::BEGIN::SEQUENCE_INTRO;
+        for ( size_t index = 0; index < command.codes.size(); ++index )
+        {
+            output << (int)command.codes[index];
+            if ( index < command.codes.size() - 1 ) output << ANSI::COMMAND::CODE::DELIMITER;
+        }
+        output << command.closing_tag;
+        return output;
+    }
+};
+
+// namespace ANSI

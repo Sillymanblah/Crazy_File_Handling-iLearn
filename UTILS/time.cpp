2ad62ee594eaaa34377d8a26096f07a02b2bd3a5
John Grzegorczyk <jtgrzeg@gmail.com>
2024-11-01 20:05:14 -0500

    More work on time class
    Adding more functionality to the class and working on the set_time() function.
    Adds a whole mess of static inline values for easier usage.

--- UTILS/time.cpp
+++ UTILS/time.cpp
@@ -9,33 +9,115 @@ bool time::is_leap_year( short_time __year )
 time::short_time time::get_days_in_year( short_time __year )
 { return is_leap_year( __year ) ? days_in_year + 1 : days_in_year; }
 
-
+// February has one more day on leap years.
 time::short_time time::get_days_in_month( short_time __month, short_time __year )
 { return ( __month == month::February ) ? ( is_leap_year( __year ) ? max_days[ __month ] + 1 : max_days[ __month ] ) : max_days[ __month ]; }
 
-void time::set_time( const std::string& __str_time )
+// Finds the month based on number of days into the year, and leaves __days as the number of days remaining (so days in the month)
+time::short_time time::find_month( short_time& __days )
 {
-    // still need to write!
+    for ( short_time month = 0; month < max_months; ++month )
+    {
+        if ( __days > max_days[ month ] ) __days -= max_days[ month ];
+        else return month;
+    }
 }
 
-void time::set_time( time_value __time )
+void time::break_raw_time( const raw_time& __time, const precision& __precision, time_breakdown& __parts )
 {
-    // still need to write!
+    switch ( __precision )
+    {
+    case precision::years:
+        if ( __time > max_years ) throw std::invalid_argument( "The raw time entered has a time larger than the maximum allowed time!" );
+        __parts[ breakdown_year ] = static_cast< short_time >( __time );
+        break;
+
+    case precision::months:
+        break_raw_time( __time / max_months, precision::years, __parts );
+        __parts[ breakdown_month ] = static_cast< short_time >( __time % max_months );
+        break;
+
+    case precision::days:
+        break_raw_time( __time / average_days_in_year, precision::years, __parts );
+        __parts[ breakdown_month ] = find_month( __parts[ breakdown_day ] = __time - __parts[ breakdown_year ] * average_days_in_year );
+        break;
+
+    case precision::hours:
+        break_raw_time( __time / max_hours, precision::days, __parts );
+        __parts[ breakdown_hour ] = static_cast< short_time >( __time % max_hours );
+        break;
+
+    case precision::minutes:
+        break_raw_time( __time / max_minutes, precision::hours, __parts );
+        __parts[ breakdown_hour ] = static_cast< short_time >( __time % max_minutes );
+        break;
+
+    case precision::seconds:
+        break_raw_time( __time / seconds_per_minute, precision::minutes, __parts );
+        break;
+
+    case precision::deciseconds:
+        break_raw_time( __time / deci, precision::seconds, __parts );
+        break;
+
+    case precision::centiseconds:
+        break_raw_time( __time / centi, precision::seconds, __parts );
+        break;
+
+    case precision::milliseconds:
+        break_raw_time( __time / milli, precision::seconds, __parts );
+        break;
+
+    default:
+        throw std::invalid_argument( "The value of precision seems to be invalid!" );
+    }
 }
 
-time::short_time time::isolate( const time_value& __isolator, const unsigned char& bit_shift )
-{ return (_time & __isolator) >> bit_shift; }
+time::short_time time::build_time( const time_breakdown& __parts )
+{ return __parts[ breakdown_year ] << year_shift | __parts[ breakdown_month ] << month_shift | __parts[ breakdown_day ] << day_shift | __parts[ breakdown_hour ] << hour_shift | __parts[ breakdown_minute ]; }
 
-time::short_time time::get_year()
-{ return isolate( year_mask, year_shift ); }
-time::short_time time::get_month()
-{ return isolate( month_mask, month_shift ); }
-time::short_time time::get_day()
-{ return isolate( day_mask, day_shift ); }
-time::short_time time::get_hour()
-{ return isolate( hour_mask, hour_shift ); }
-time::short_time time::get_minute()
-{ return isolate( minute_mask ); }
+void time::set_time( const raw_time& __time, const precision& __precision )
+{
+    time_breakdown parts;
+    break_raw_time( __time, __precision, parts );
+    _time = build_time( parts );
+}
+
+time::short_time time::isolate( const time_value& __time, const time_value& __mask, const byte& __bitshift )
+{ return (__time & __mask) >> __bitshift; }
+
+time::short_time time::get_year( const time_value& __time )
+{ return isolate( __time, year_mask, year_shift ); }
+time::short_time time::get_month( const time_value& __time )
+{ return isolate( __time, month_mask, month_shift ); }
+time::short_time time::get_day( const time_value& __time )
+{ return isolate( __time, day_mask, day_shift ); }
+time::short_time time::get_hour( const time_value& __time )
+{ return isolate( __time, hour_mask, hour_shift ); }
+time::short_time time::get_minute( const time_value& __time )
+{ return isolate( __time, minute_mask ); }
+
+time::short_time time::get_time( const precision& __precision )
+{
+    switch ( __precision )
+    {
+    case precision::years: return get_year( _time );
+    case precision::months: return get_month( _time );
+    case precision::days: return get_day( _time );
+    case precision::hours: return get_hour( _time );
+    case precision::minutes: return get_minute( _time );
+
+    // We do not store values for seconds or below.
+    case precision::seconds:
+    case precision::milliseconds:
+    case precision::centiseconds:
+    case precision::deciseconds:
+        return 0;
+
+    default:
+        throw std::invalid_argument( "The value of precision seems to be invalid!" );
+    }
+}
 
 std::string time::get_am_pm( short_time __hour )
 {
@@ -66,21 +148,60 @@ std::string time::get_month( short_time __month )
 std::string time::get_short_month( short_time __month )
 { return get_month( __month ).substr( 0, 3 ); }
 
-std::string time::construct_time_string( const std::array< short_time, 5 >&& __parts )
+std::string time::construct_time_string( const time_breakdown&& __parts )
 {
     using std::to_string;
     const auto& [ year, month, day, hour, minute ] = __parts;
 
-    return get_short_month( month ) + ' ' + to_string( day )
-        + ", " + to_string( year ) + ' ' + to_string( hour % 12 )
-        + to_string( minute ) + ' ' + get_am_pm( hour );
+    return get_short_month( month ) + ' ' + to_string( day + 1 )
+        + ", " + to_string( year ) + ' ' + to_string( (hour - 1) % 12 + 1 )
+        + ':' + to_string( minute ) + ' ' + get_am_pm( hour );
 }
 
-std::array< time::short_time, 5 > time::break_time()
-{ return { get_year(), get_month(), get_day(), get_hour(), get_minute() };}
+time::short_time time::days_before_month( short_time __month )
+{
+    short_time total_days = 0;
+    for ( short_time month = 0; month < __month; ++month )
+        total_days += max_days[ month ];
+    return total_days;
+}
 
-std::string time::get_time()
-{ return construct_time_string( break_time() ); }
+time::raw_time time::construct_raw_time( const time_breakdown&& __parts, const precision& __precision )
+{
+    raw_time time_val = 0;
+    const auto& [ year, month, day, hour, minute ] = __parts;
 
-time::time_value time::raw_time()
-{ return _time; }
+    switch ( __precision )
+    {
+        case precision::years:
+            return year;
+        case precision::months:
+            return year * max_months + month;
+        case precision::days:
+            return year * average_days_in_year + days_before_month( month ) + day;
+        case precision::hours:
+            return ( year * average_days_in_year + days_before_month( month ) + day ) * max_hours + hour; // If we switch to recursion this will look cleaner.
+        case precision::minutes:
+            return ( ( year * average_days_in_year + days_before_month( month ) + day ) * max_hours + hour ) * max_minutes + minute;
+        case precision::seconds:
+            return ( ( ( year * average_days_in_year + days_before_month( month ) + day ) * max_hours + hour ) * max_minutes + minute ) * seconds_per_minute;
+        case precision::deciseconds:
+            return ( ( ( year * average_days_in_year + days_before_month( month ) + day ) * max_hours + hour ) * max_minutes + minute ) * seconds_per_minute * deci;
+        case precision::centiseconds:
+            return ( ( ( year * average_days_in_year + days_before_month( month ) + day ) * max_hours + hour ) * max_minutes + minute ) * seconds_per_minute * centi;
+        case precision::milliseconds:
+            return ( ( ( year * average_days_in_year + days_before_month( month ) + day ) * max_hours + hour ) * max_minutes + minute ) * seconds_per_minute * milli;
+    
+        default:
+            throw std::invalid_argument( "The value of precision seems to be invalid!" );
+    }
+}
+
+time::time_breakdown time::break_time( const time_value& __time )
+{ return { get_year( __time ), get_month( __time ), get_day( __time ), get_hour( __time ), get_minute( __time ) };}
+
+std::string time::get_time_string()
+{ return construct_time_string( break_time( _time ) ); }
+
+time::raw_time time::get_raw_time( const precision& __precision )
+{ return construct_raw_time( break_time( _time ), __precision ); }
--- UTILS/time.hpp
+++ UTILS/time.hpp
@@ -8,12 +8,15 @@
 class time
 {
 private:
-    using short_time        = unsigned int;
+    using short_time        = unsigned short;
     using time_value        = unsigned long;
+    using byte              = unsigned char;
 
-public:
     // Must be: years, months, days, hours, minutes.
     using time_breakdown    = std::array< short_time, 5 >;
+
+public:
+    using raw_time          = time_t;
 
 private:
     enum month
@@ -33,6 +36,20 @@ private:
     };
 
 public:
+    enum class precision
+    {
+        milliseconds,
+        centiseconds,
+        deciseconds,
+        seconds,
+        minutes,
+        hours,
+        days,
+        months,
+        years,
+    };
+
+public:
     time() : _time() {}
     time( const time& ) = default;
     time( time&& ) = default;
@@ -45,54 +62,78 @@ private:
     constexpr static inline short_time get_days_in_year( short_time __year );
     constexpr static inline short_time get_days_in_month( short_time __month, short_time __year );
 
+    constexpr static inline short_time find_month( short_time& __days );
+    constexpr static inline void break_raw_time( const raw_time& __time, const precision& __precision, time_breakdown& __parts );
+    constexpr static inline short_time build_time( const time_breakdown& __parts );
+
 public:
-    void set_time( const std::string& __str_time ); // Definitely need helpers for this function!
-    void set_time( time_value __time );
+    void set_time( const raw_time& __time, const precision& __precision = precision::seconds );
 
 private:
-    short_time isolate( const time_value& __isolator, const unsigned char& bitshift = 0 );
+    constexpr static inline short_time isolate( const time_value& __time, const time_value& __mask, const byte& __bitshift = no_shift );
+
+    constexpr static inline short_time get_year( const time_value& __time );
+    constexpr static inline short_time get_month( const time_value& __time );
+    constexpr static inline short_time get_day( const time_value& __time );
+    constexpr static inline short_time get_hour( const time_value& __time );
+    constexpr static inline short_time get_minute( const time_value& __time );
 
 public:
-    short_time get_year();
-    short_time get_month();
-    short_time get_day();
-    short_time get_hour();
-    short_time get_minute();
-
+    short_time get_time( const precision& __precision );
 
 private:
     static inline std::string get_am_pm( short_time __hour );
     static inline std::string get_month( short_time __month );
     static inline std::string get_short_month( short_time __month );
-
     static inline std::string construct_time_string( const time_breakdown&& __parts );
-    time_breakdown break_time();
+
+    static inline short_time days_before_month( short_time month );
+    static inline raw_time construct_raw_time( const time_breakdown&& __parts, const precision& __precision );
+
+    static inline time_breakdown break_time( const time_value& __time );
 
 public:
-    std::string get_time();
-    time_value raw_time();
+    std::string get_time_string();
+    raw_time get_raw_time( const precision& __precision = precision::seconds );
 
 private:
     time_value _time;
 
     // This is probably the easiest way to handle these values:
+    // NOTE: These could also be enumerated values. Might refactor later.
 
     constexpr static inline short_time      max_years               = 65535;        // Permitted max years
     constexpr static inline time_value      year_mask               = 0x0FF00000;   // 16 bits required
-    constexpr static inline unsigned char   year_shift              = 20;           // Num bits to the right
+    constexpr static inline byte            year_shift              = 20;           // Num bits to the right
+    constexpr static inline byte            breakdown_year          = 0;            // Index of time_breakdown
+
     constexpr static inline short_time      max_months              = 12;           // Months in a year
     constexpr static inline time_value      month_mask              = 0x000F0000;   // 4 bits required
-    constexpr static inline unsigned char   month_shift             = 16;           // Num bits to the right
+    constexpr static inline byte            month_shift             = 16;           // Num bits to the right
+    constexpr static inline byte            breakdown_month         = 1;            // Index of time_breakdown
+
     constexpr static inline short_time      max_days[ max_months ]  = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 }; // Max days per month for non-leap-years
     constexpr static inline time_value      day_mask                = 0x0000F800;   // 5 bits required
-    constexpr static inline unsigned char   day_shift               = 11;           // Num bits to the right
+    constexpr static inline byte            day_shift               = 11;           // Num bits to the right
+    constexpr static inline byte            breakdown_day           = 2;            // Index of time_breakdown
+
     constexpr static inline short_time      max_hours               = 24;           // Hours in a day
     constexpr static inline time_value      hour_mask               = 0x000007C0;   // 5 bits required
-    constexpr static inline unsigned char   hour_shift              = 6;            // Num bits to the right
+    constexpr static inline byte            hour_shift              = 6;            // Num bits to the right
+    constexpr static inline byte            breakdown_hour          = 3;            // Index of time_breakdown
+
     constexpr static inline short_time      max_minutes             = 60;           // Minutes in an hour
     constexpr static inline time_value      minute_mask             = 0x0000003F;   // 6 bits required
+    constexpr static inline byte            no_shift                = 0;            // No bit shift
+    constexpr static inline byte            breakdown_minute        = 4;            // Index of time_breakdown
 
     constexpr static inline short_time      days_in_year            = 365;          // Total for non-leap-years
+    constexpr static inline float           average_days_in_year    = 365.2425;     // Total for non-leap-years
+
+    constexpr static inline short_time      seconds_per_minute      = 60;           // Used to convert time to seconds
+    constexpr static inline short_time      deci                    = 10;           // For deeper time precision
+    constexpr static inline short_time      centi                   = 100;          // For even deeper time precision
+    constexpr static inline short_time      milli                   = 1000;         // For deepest time precision
 };
 
 #endif // TIME_HPP

2d5c3944f07a9351800b0e5aef5a5dcbb18b64da
John Grzegorczyk <jtgrzeg@gmail.com>
2024-11-15 12:57:25 -0600

    Updated aliases for clarity

--- functional_parsing/parser.hpp
+++ functional_parsing/parser.hpp
@@ -5,14 +5,14 @@ template < class _Elem, class _Storage, class... _Types >
 class parser
 {
 public:
-    using _Stream = std::basic_fstream< _Elem >;
-    using _String = std::basic_string< _Elem >;
+    using _MyStream = std::basic_fstream< _Elem >;
+    using _MyString = std::basic_string< _Elem >;
 
-    using read_function = _Stream& (*)( _Stream&, _String& );
-    using skip_function = _Stream& (*)( _Stream& );
+    using read_function = _MyStream& (*)( _MyStream&, _MyString& );
+    using skip_function = _MyStream& (*)( _MyStream& );
 
     template < class _Ty > // Enforce that the type here is contained in (_Types...).
-    using conversion_function = _Ty (*)( const _String& );
+    using conversion_function = _Ty (*)( const _MyString& );
     using store_function = void (*)( _Storage, _Types... );
     using emplace_function = void (*)( _Storage, _Types... );
     template < class _Ty > // Enforce that the type here is the type inside of (_Storage).
@@ -23,7 +23,7 @@ public:
     parser( const parser& );
     parser( parser&& );
 
-    _Storage parse( _Stream& __filestream );
+    _Storage parse( _MyStream& __filestream );
 
     _Storage parse( const std::string& __filename );
     _Storage parse( std::string&& __filename );
